91e18c723b888770a99a3805f55b21e0ee66a7d6
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2025-05-01 01:46:26 +0330

    Create PendingUnwraps.ts

---
+++ src/components/PendingUnwraps.ts
@@ -0,0 +1,95 @@
+import { Signature } from 'hive-tx'
+import { Transaction } from 'hive-tx'
+import { operatorKeys } from '../helpers/general/get_operators.ts'
+import { call } from 'hive-tx'
+
+class PendingUnwraps {
+	private multisigTreshold = 1 // We should get this from the API
+	private unwraps: Map<string, Transaction> = new Map()
+
+	constructor() {
+		setInterval(() => {
+			this.unwraps.forEach(async (trx, ethTrxHash) => {
+				if (trx.signedTransaction) {
+					const signatures = trx.signedTransaction.signatures
+					if (signatures.length >= this.multisigTreshold) {
+						try {
+							const res = await trx.broadcast()
+							console.log(res)
+						} catch (e) {
+							console.log(e)
+							// Should fail due to duplicate transaction error if broadcasted by other peers
+						}
+						this.unwraps.delete(ethTrxHash)
+					}
+				}
+			})
+		}, 5_000)
+
+		// Check and remove already unwrapped trxs
+		setInterval(async () => {
+			for (const [k, trx] of this.unwraps) {
+				const res = await call('condenser_api.get_transaction', [
+					trx.digest().txId,
+				])
+				if (!res?.result) {
+					continue
+				}
+				this.unwraps.delete(k)
+			}
+		}, 20_000)
+	}
+
+	public async addUnwrap(ethTrxHash: string, trx: Transaction) {
+		// Skip already broadcasted transactions
+		const res = await call('condenser_api.get_transaction', [trx.digest().txId])
+		if (!res?.result) {
+			this.unwraps.set(ethTrxHash, trx)
+		}
+	}
+
+	public getUnwrap(ethTrxHash: string) {
+		return this.unwraps.get(ethTrxHash)
+	}
+
+	/** Verify the signature and add to the trx */
+	public addSignature(
+		operator: string,
+		ethTrxHash: string,
+		signature: string,
+		retry = 1,
+	) {
+		try {
+			const trx = this.unwraps.get(ethTrxHash)
+			if (trx) {
+				const sig = Signature.from(signature)
+				const recoveredKey = sig.getPublicKey(trx.digest().digest).toString()
+				const operatorKey = operatorKeys.get(operator)
+				if (!operatorKey) {
+					return
+				}
+				for (let i = 0; i < operatorKey.length; i++) {
+					if (recoveredKey === operatorKey[i]) {
+						trx.addSignature(signature)
+						break
+					}
+				}
+			} else {
+				if (retry) {
+					// We might have not synced the transaction yet, wait and try one more time
+					setTimeout(() => {
+						this.addSignature(operator, ethTrxHash, signature, 0)
+					}, 30_000)
+				}
+			}
+		} catch {
+			// Ignore badly formatted signatures
+		}
+	}
+
+	public getAllUnwraps() {
+		return this.unwraps
+	}
+}
+
+export const pendingUnwraps = new PendingUnwraps()
