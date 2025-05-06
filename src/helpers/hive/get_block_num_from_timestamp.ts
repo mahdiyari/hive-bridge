843499924108dfaea2173507b8fa76cc1982ad3a
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2025-05-06 16:18:46 +0330

    Update get_block_num_from_timestamp.ts

--- src/helpers/hive/get_block_num_from_timestamp.ts
+++ src/helpers/hive/get_block_num_from_timestamp.ts
@@ -15,38 +15,38 @@ const errorMargin = 15 * 60 * 1000
  * We need deterministic results for generating deterministic transactions across the network
  */
 export const getBlockNumFromTimestamp = async (timestamp: number) => {
-	// Round the timestamp to the start of hour
-	const targetTimestamp = startOfHour(timestamp)
+	// Round the timestamp to the start of minute
+	const targetTimestamp = startOfMinute(timestamp)
 	if (cache.has(targetTimestamp)) {
 		return <number> cache.get(targetTimestamp)
 	}
-	let delta = knownTimestamp - targetTimestamp
+	let delta = knownTimestamp - targetTimestamp // -number
 	let estimatedBlockNum = knownBlock - Math.round(delta / 3000)
 	let foundTimestamp = await getBlockTimestamp(estimatedBlockNum)
-	while (foundTimestamp - targetTimestamp >= errorMargin) {
-		delta = foundTimestamp - targetTimestamp
-		estimatedBlockNum -= Math.round(delta / 3000)
-		foundTimestamp = await getBlockTimestamp(estimatedBlockNum)
+	while (Math.abs(foundTimestamp - targetTimestamp) >= errorMargin) {
 		while (foundTimestamp === 0) {
 			// in case we overshoot
 			estimatedBlockNum -= 1000
 			foundTimestamp = await getBlockTimestamp(estimatedBlockNum)
 		}
+		delta = foundTimestamp - targetTimestamp // -number
+		estimatedBlockNum -= Math.round(delta / 3000)
+		foundTimestamp = await getBlockTimestamp(estimatedBlockNum)
 	}
 	cache.set(targetTimestamp, estimatedBlockNum)
 	return estimatedBlockNum
 }
 
-const startOfHour = (timestamp: number) => {
+const startOfMinute = (timestamp: number) => {
 	const date = new Date(timestamp)
-	date.setMinutes(0, 0, 0)
+	date.setSeconds(0, 0)
 	return date.getTime()
 }
 
 const getBlockTimestamp = async (blockNum: number) => {
 	const res = await call('condenser_api.get_block_header', [blockNum])
 	if (res?.result?.timestamp) {
-		return new Date(res.result.timestamp).getTime()
+		return new Date(res.result.timestamp + '.000Z').getTime()
 	} else {
 		return 0
 	}
