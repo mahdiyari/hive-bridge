8f47c0bcc92fc03411274c011b86de560b457917
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2025-11-09 11:21:02 +0330

    Create HiveService.ts

---
+++ src/blockchain/hive/HiveService.ts
@@ -0,0 +1,107 @@
+import { call, config as configHiveTx } from 'hive-tx'
+import { TransferBody, TransferHistory } from '@/types/hive.types'
+import { ethers } from 'ethers'
+import { config } from '@/config'
+import { ChainName, ChainService } from '@/types/chain.types'
+
+export class HiveService {
+  private MIN_AMOUNT = 1
+  private POLLING_INTERVAL = 5_000 // 5s
+  private TREASURY = config.hive.treasury
+  private lastIrreversibleBlockNum = 0
+  private event = new EventTarget()
+  private nodeIndex = 0
+  private nodes = config.hive.nodes
+  private lastHistoryId = 0
+  private genesisBlock
+
+  constructor(genesis: number) {
+    this.genesisBlock = genesis
+    configHiveTx.node = this.nodes
+  }
+
+  /** Start HiveService */
+  public start() {
+    this.processHistory()
+  }
+
+  /** Triggers on transfers to the bridge account with valid memo */
+  public onTransfer(cb: (detail: TransferBody) => void) {
+    this.event.addEventListener('transfer', (e) => {
+      const pe = e as CustomEvent
+      cb(pe.detail)
+    })
+  }
+
+  private async processHistory(count = 1000) {
+    let transfers = await this.getTransferHistory(-1, count)
+    let len = transfers.length
+    if (!transfers || !len) {
+      return
+    }
+    // No new items in the history
+    if (transfers[len - 1][0] <= this.lastHistoryId) {
+      return
+    }
+    // fetch all items as long as not already processed
+    while (
+      len === count &&
+      transfers[0][0] > this.lastHistoryId &&
+      transfers[0][1].block >= this.genesisBlock
+    ) {
+      // history includes the start item as well so we don't want that again
+      const start = transfers[0][0] - 1
+      const temp = await this.getTransferHistory(start, count)
+      len = temp.length
+      transfers = temp.concat(transfers)
+    }
+    for (let i = 0; i < transfers.length; i++) {
+      const historyId = transfers[i][0]
+      const blockNum = transfers[i][1].block
+      const timestamp = new Date(transfers[i][1].timestamp + '.000Z').getTime()
+      const trxId = transfers[i][1].trx_id
+      const opInTrx = transfers[i][1].op_in_trx
+      // We have already proccessed till lastHistoryId
+      if (historyId <= this.lastHistoryId || blockNum < this.genesisBlock) {
+        continue
+      }
+      const opBody = transfers[i][1].op[1]
+      if (opBody.to !== this.TREASURY) {
+        // Outgoing transfers
+      }
+      if (opBody.to === this.TREASURY) {
+        const asset = opBody.amount.split(' ')
+        // Accept only amounts >= MIN_AMOUNT HIVE/HBD
+        const memoSplit = opBody.memo.split(':')
+        if (memoSplit.length !== 2) {
+          continue
+        }
+        // Validate amount then dispatch a custom event for onTransfer()
+        if (Number(asset[0]) >= this.MIN_AMOUNT) {
+          const customEvent = new CustomEvent('transfer', {
+            detail: { ...opBody, blockNum, timestamp, trxId, opInTrx },
+          })
+          this.event.dispatchEvent(customEvent)
+        }
+      }
+    }
+    this.lastHistoryId = transfers[transfers.length - 1][0]
+
+    // Run only once
+    if (count === 1000) {
+      setInterval(() => {
+        this.processHistory(10)
+      }, this.POLLING_INTERVAL)
+    }
+  }
+
+  private async getTransferHistory(start = -1, count = 1000) {
+    const result = await call('condenser_api.get_account_history', [
+      this.TREASURY,
+      start,
+      count,
+      4,
+    ])
+    return <TransferHistory[]>result.result
+  }
+}
