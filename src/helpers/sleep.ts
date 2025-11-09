d62c248b2bbd1695af69cee7740c2026d81ec241
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2025-11-09 11:21:40 +0330

    restructure

--- src/helpers/sleep.ts
+++
@@ -1,8 +0,0 @@
-/** Sleep for time (ms) */
-export const sleep = (time: number): Promise<void> => {
-  return new Promise((res, _rej) => {
-    setTimeout(() => {
-      res()
-    }, time)
-  })
-}
