fd109b860d6e18624cf45980d490527bfaa789be
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2025-12-25 20:54:49 +0330

    Update msgHash.ts

--- src/governance/msgHash.ts
+++ src/governance/msgHash.ts
@@ -5,27 +5,34 @@ import { getPublicActiveKeys } from '@/utils/hive.utils'
 export const getChainMessageHash = async (
   chain: ChainService,
   proposal: Proposal,
-  sig = false
+  sign = false
 ): Promise<string> => {
   let hash = ''
   switch (proposal.method) {
     case 'add-signer':
-      const pubKeys = await getPublicActiveKeys(proposal.target!)
+      const pubKeys = await getPublicActiveKeys(proposal.target)
       const address = chain.toAddress(pubKeys[0])
-      hash = await chain.hashAddSignerMsg(proposal.target!, address)
+      hash = await chain.hashAddSignerMsg(
+        proposal.target,
+        address,
+        proposal.nonce
+      )
       break
     case 'remove-signer':
-      hash = await chain.hashRemoveSignerMsg(proposal.target!)
+      hash = await chain.hashRemoveSignerMsg(proposal.target, proposal.nonce)
       break
     case 'update-threshold':
-      hash = await chain.hashUpdateMultisigThresholdMsg(Number(proposal.target))
+      hash = await chain.hashUpdateMultisigThresholdMsg(
+        Number(proposal.target),
+        proposal.nonce
+      )
       break
     case 'pause':
-      hash = await chain.hashPauseMsg()
+      hash = await chain.hashPauseMsg(proposal.nonce)
       break
     case 'unpause':
-      hash = await chain.hashUnPauseMsg()
+      hash = await chain.hashUnPauseMsg(proposal.nonce)
       break
   }
-  return sig ? chain.signMsgHash(hash) : hash
+  return sign ? chain.signMsgHash(hash) : hash
 }
