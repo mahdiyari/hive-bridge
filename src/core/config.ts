88eb5c76f58c54c5ce32c44f480e6e70744eb5fb
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2026-08-20 21:31:09 +0330

    try another sepolia node

--- src/core/config.ts
+++ src/core/config.ts
@@ -52,7 +52,7 @@ export const config = {
       nodes: [
         'https://ethereum-sepolia-rpc.publicnode.com',
         // 'https://ethereum-sepolia-public.nodies.app',
-        'https://api.zan.top/eth-sepolia',
+        'https://lb.routeme.sh/rpc/evm/11155111',
       ],
     },
     mainnet: {
